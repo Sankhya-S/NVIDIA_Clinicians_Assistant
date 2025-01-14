56f52d4d3854dd1bec91fb37f6bffa30cced55b7
Margo-Kim <142247799+Margo-Kim@users.noreply.github.com>
2025-01-14 10:29:29 -0600

    Delete notes_processing.py
    no longer needed

--- notes_processing.py
+++
@@ -1,9 +0,0 @@
-from PyPDF2 import PdfReader
-
-def extract_text_from_pdf(pdf_path):
-    """Extracts text from a PDF file using PyPDF2."""
-    reader = PdfReader(pdf_path)
-    text = ""
-    for page in reader.pages:
-        text += page.extract_text()
-    return text
